48dca8f0ceef20878922bc2af4a3b85b8433b985
Matt Filetto <matt.filetto@gmail.com>
2024-01-27 06:26:01 -0800

    shusky: override first API level build property.
    * Anything above 32 causes hardware enforcement. This
      allows us to continue to pass CTS checks.
    
    Change-Id: I4113b7378cd1a6c8cc6a553d0672833bb2285cfd
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,3 +3,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
+DEVICE_PATH := device/google/shusky
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_shusky
+
