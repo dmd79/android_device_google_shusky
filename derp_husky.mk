7e60285794bf705b318cc16a63f7c7fe3439816a
dmd79 <davidevinavil@gmail.com>
2024-02-13 10:52:58 +0100

    shusky: derp

---
+++ derp_husky.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+DERP_BUILDTYPE := Official
+
+# Inherit device configuration
+$(call inherit-product, device/google/shusky/aosp_husky.mk)
+$(call inherit-product, device/google/zuma/lineage_common.mk)
+
+include device/google/shusky/husky/device-lineage.mk
+
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_8 := true
+WITH_GMS := true
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 8 Pro
+PRODUCT_NAME := derp_husky
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2992
+TARGET_SCREEN_WIDTH := 1344
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=husky \
+    PRIVATE_BUILD_DESC="husky-user 14 AP1A.240405.002 11480754 release-keys"
+
+BUILD_FINGERPRINT := google/husky/husky:14/AP1A.240405.002/11480754:user/release-keys
+
+$(call inherit-product, vendor/google/husky/husky-vendor.mk)
---
+++ derp_shiba.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+DERP_BUILDTYPE := Official
+
+# Inherit device configuration
+$(call inherit-product, device/google/shusky/aosp_shiba.mk)
+$(call inherit-product, device/google/zuma/lineage_common.mk)
+
+include device/google/shusky/shiba/device-lineage.mk
+
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_8 := true
+WITH_GMS := true
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 8
+PRODUCT_NAME := derp_shiba
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=shiba \
+    PRIVATE_BUILD_DESC="shiba-user 14 AP1A.240405.002 11480754 release-keys"
+
+BUILD_FINGERPRINT := google/shiba/shiba:14/AP1A.240405.002/11480754:user/release-keys
+
+$(call inherit-product, vendor/google/shiba/shiba-vendor.mk)
