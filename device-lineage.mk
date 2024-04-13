a1bf015417e114571ad05707935b40edde60ef28
Ionut Gherman <ghermanionut96@gmail.com>
2024-04-13 22:17:55 +0300

    shusky: Include Google Face Unlock
    Change-Id: I22c4813d905bbeffd02e30597f191d31a57f73e7

--- device-lineage.mk
+++ device-lineage.mk
@@ -31,6 +31,9 @@ include hardware/google/pixel/powershare/device.mk
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
 
+# Face Unlock
+include vendor/google/faceunlock/device.mk
+
 # Build necessary packages for vendor
 
 # Audio
